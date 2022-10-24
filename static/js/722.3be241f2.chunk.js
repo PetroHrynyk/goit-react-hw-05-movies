e972fde831a532055b818a1e4ad69936d5cd6c0c
PetroHrynyk <97837422+PetroHrynyk@users.noreply.github.com>
2022-10-24 16:14:21 +0000

    Deploying to gh-pages from @ PetroHrynyk/goit-react-hw-05-movies@5f8cd0fe7320e3427d6a95211d360e5e5007a445 🚀

---
+++ static/js/722.3be241f2.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_05_movies=self.webpackChunkgoit_react_hw_05_movies||[]).push([[722],{722:function(e,n,t){t.r(n),t.d(n,{default:function(){return r}});var c=t(731),i=t(184);function r(){return(0,i.jsxs)("div",{className:"container",children:[(0,i.jsx)("h1",{children:"Page not found"}),(0,i.jsx)(c.rU,{to:"/",children:"Back to Main page"})]})}}}]);
+//# sourceMappingURL=722.3be241f2.chunk.js.map
